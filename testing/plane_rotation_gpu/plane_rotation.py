a918a34838c35881144a1534a9abf89d0c5694e6
Brayden O'Neal <braydenoneal@protonmail.com>
2024-12-10 14:40:24 -0600

    Plane rotation gpu work in progress

---
+++ testing/plane_rotation_gpu/plane_rotation.py
@@ -0,0 +1,47 @@
+import numpy as np
+from skimage import io, color
+from max_loss_normal import get_max_loss_normal
+import matplotlib.pyplot as plt
+import torch
+from torch.backends import mps
+
+device = 'cuda' if torch.cuda.is_available() else 'mps' if mps.is_available() else 'cpu'
+
+tensor = lambda x: torch.tensor(x, dtype=torch.float32, device=device)
+
+dichromat_angles = {
+    'protanopia': 11.48,
+    'deuteranopia': 8.11,
+    'tritanopia': -46.37,
+}
+
+dichromat_angle = tensor(np.radians(dichromat_angles['protanopia']))
+image_path = 'images/boat_screenshot.png'
+
+original_image = color.rgb2lab(io.imread(image_path)[:, :, :3])
+original_image = tensor(original_image)
+
+# Project image to dichromat plane
+dichromat_normal = tensor([0, torch.cos(dichromat_angle), torch.sin(dichromat_angle)])
+dichromat_image = original_image - original_image[:, :, torch.newaxis] @ dichromat_normal * dichromat_normal
+
+# Compute maximum contrast loss angle
+max_loss_normal = get_max_loss_normal(original_image, dichromat_image)
+
+# Project image to maximum contrast loss plane
+recolored_image = original_image - np.dot(original_image[:, :, np.newaxis], max_loss_normal) * max_loss_normal
+
+# Rotate to align with dichromat plane
+angle = np.arccos(np.dot(dichromat_normal, max_loss_normal)) + np.pi
+transform_matrix = ((1, 0, 0), (0, np.cos(angle), -np.sin(angle)), (0, np.sin(angle), np.cos(angle)))
+recolored_image = np.dot(recolored_image, transform_matrix)
+
+# Display images
+fig, axes = plt.subplots(1, 3)
+plt.setp(axes, xticks=[], yticks=[])
+
+axes[0].imshow(color.lab2rgb(original_image))
+axes[1].imshow(color.lab2rgb(dichromat_image))
+axes[2].imshow(color.lab2rgb(recolored_image))
+
+plt.show()
